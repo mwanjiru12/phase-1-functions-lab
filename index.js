14c959d97ac8bd1705e629b7b8b3f33dc2305549
Maria Kamau <maria.kamau@student.moringaschool.com>
2024-03-20 21:31:09 +0300

    functions lab

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// Code your solution in this file!
+//Code your solution in this file!
+//Returns the number of blocks from Scuber's headquarters to the pickup location.
 function distanceFromHqInBlocks(blockNumber) {
   if (blockNumber > 42) {
     return blockNumber - 42;
@@ -7,11 +8,15 @@ function distanceFromHqInBlocks(blockNumber) {
   }
 }
 
-
+//Returns the number of feet from Scuber's headquarters to the pickup location. 
+//Uses `distanceFromHqInBlocks` function to help return the correct value here. 
 function distanceFromHqInFeet(blockNumber) {
   return distanceFromHqInBlocks(blockNumber) * 264;
 }
 
+//Calculates the number of feet a passenger travels given a starting block and an ending block — 
+//it only calculates distance North and South (uptown/downtown). 
+//It uses the knowledge that a block is 264 feet long.
 function distanceTravelledInFeet(start, destination) {
   if (start < destination) {
     return (destination - start) * 264;
@@ -20,6 +25,10 @@ function distanceTravelledInFeet(start, destination) {
   }
 }
 
+//Given the same starting and ending block as the previous test (hint hint), return the fare for the customer. 
+//The first four hundred feet are free. For a distance between 400 and 2000 feet, the price is 2 cents per foot (not including 400, which are free!). 
+//Then Scuber charges a flat fare of $25 for a distance over 2000 feet and under 2500 feet. Finally, 
+//Scuber does not allow any rides over 2500 feet — the function returns 'cannot travel that far' if a ride over 2500 feet is requested.
 function calculatesFarePrice(start, destination) {
   const distance = distanceTravelledInFeet(start, destination);
 
